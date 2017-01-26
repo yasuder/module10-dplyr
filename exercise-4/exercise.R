2848161bd14c9a81b392a665a9ca7ea054381794
Erika Yasuda <eri.yasuda222@gmail.com>
2017-01-26 14:12:53 -0800

    Add complete exercise 4 and exercise 8 with additional csv files created

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -1,36 +1,52 @@
 # Exercise 4: DPLYR and flights data
 
-# Install the nycflights13 package and read it in.  Require the dplyr package
-# install.packages("nycflights13")
+# Install the `nycflights13` package. Load (`library()`) the package.
+# You'll also need to load `dplyr`
+install.packages("nycflights13")
+library(nycflights13)
+library(dplyr)
 
+# The data.frame `flights` should now be accessible to you.
+# Use functions to inspect it: how many rows and columns does it have?
+# What are the names of the columns?
+# use `??flights` to search for documentation on the data set (for what cols are)
+View(flights)
 
-# The data.frame flights should now be accessible to you.  View it,
-# and get some basic information about the number of rows/columns
+# Use `dplyr` to give the data frame a new column that is the amount of time gained in the air
+## flights <- mutate(flights, gain = arr_delay - dep_delay)
 
+# Use `dplyr` to sort your data.frame descending by the column you just created.
+# Remember to save this as a variable (or in the same one!)
+## flights <- arrange(flights, desc(gain))
 
-# Add a column that is the amount of time gained in the air
+# For practice, repeat the last 2 steps in a single statement using the pipe operator
+flights <- mutate(flights, gain = arr_delay - dep_delay) %>% 
+  arrange(desc(gain))
 
+# Make a histogram of the amount of gain using the `hist()` function
 
-# Sort your data.frame desceding by the column you just created
-
-
-# Try doing the last 2 steps in a single operation using the pipe operator
-
-
-# Make a histogram of the amount of gain using the `hist` command
-
+histogram.data <- hist(flights$gain)
 
 # On average, did flights gain or lose time?
+# Note: use the `na.rm = TRUE` argument to remove NA values from your aggregation
+mean.gain <- mean(flights$gain, na.rm = TRUE)
+print(mean.gain)
 
-
-# Create a data.frame of flights headed to seatac ('SEA'),
-# and only include the column you just created
-
+# Create a data.frame of flights headed to seatac ('SEA'), only including the
+# origin, destination, and the "gain" column you just created
+seattle <- flights %>% 
+  select(origin, dest, gain) %>%
+  filter(dest == 'SEA')
+print(seattle)
 
 # On average, did flights to seatac gain or loose time?
+avg.seattle <- mean(seattle$gain, na.rm = TRUE)
+print(avg.seattle)
 
-
-# Retireve the air_time column for flights from JFK to SEA
-
-
-# What was the average air time of those flights (in hours)?  What was the min/max?
+# Consider flights from JFK to SEA. What was the average, min, and max air time
+# of those flights? Bonus: use pipes to answer this question in one statement!
+# (and don't show any other data)
+jfk.to.sea <- flights %>% 
+  filter(origin == 'JFK' & dest == 'SEA') %>% 
+  summarise(mean = mean(air_time, na.rm = TRUE), max = max(air_time, na.rm = TRUE), min = min(air_time, na.rm = TRUE))
+print(jfk.to.sea)
--- exercise-8/exercise.R
+++ exercise-8/exercise.R
@@ -1,36 +1,54 @@
 #Use "dplyr"
 #Install.packages("dplyr")
-
-
+## getwd()
+## setwd("../module10-dplyr/exercise-8/")
 
 #Load in SwissData from data set from data folder and view it to understand what is in it.
-
+swiss <- read.csv("data/SwissData.csv", stringsAsFactors = FALSE)
 
 #Add a column (using dpylr) that is the absolute difference between Education and Examination and call it
 # Educated.Score
-
+swiss <- mutate(swiss, Education.Score = abs(Education - Examination))
 
 #Which area(s) had the largest difference
-
+max.diff <- filter(swiss, Education.Score == max(Education.Score))
+print(max.diff)
 
 #Find which region has the highest percent of men in agriculture and retunr only the
 #percent and region name.  Use pipe operators to accomplish this.
-
+max.agriculture <- filter(swiss, Agriculture == max(Agriculture)) %>% 
+  select(Region, Agriculture) 
+print(max.agriculture)
 
 #Find the average of all infant.mortality rates and create a column (Mortality.Difference)
 # showing the difference between a regions mortality rate and the mean. Arrange the dataframe in
 # Descending order based on this new column. Use pipe operators.
-
+swiss <- mutate(swiss, Mortality.Difference = Infant.Mortality - mean(Infant.Mortality)) %>% 
+  arrange(desc(Mortality.Difference))
 
 # Create a new data frame that only is that of regions that have a Infant mortality rate less than the
 # mean.  Have this data frame only have the regions name, education and mortality rate.
-
+low.mortality <- select(swiss, Region, Education, Infant.Mortality) %>% 
+  filter(Infant.Mortality < mean(Infant.Mortality))
 
 #Filter one of the columns based on a question that you may have (which regions have a higher
 #education rate, etc.) and write that to a csv file
-
+# Q: Which regions have a higher education rate?
+low.mortality <- filter(low.mortality, Education > 50)
+# write.csv(low.mortality, file = "data/low_mortality.csv")
 
 # Create a function that can take in two different region names and compare them based on a statistic
 # Of your choice (education, Examination, ect.)  print out a statment describing which one is greater
 # and return a data frame that holds the selected region and the compared variable.  If your feeling adventurous
 # also have your function write to a csv file.
+CompareExamination <- function(region1, region2) {
+  region1.exam <- filter(swiss, Region == region1) %>% 
+    select(Examination)
+  print(region1.exam)
+  region2.exam <- filter(swiss, Region == region2) %>% 
+    select(Examination)
+  max.region <- select(swiss, Examination == max(region1.exam, region2.exam))
+  is.data.frame(max.region)
+  print(paste("Of these two regions,", max.region, "has a higher examination rate."))
+}
+CompareExamination("Glane", "Broye")
